886ace59add16365effc56a7173abab9ae6996f6
tigan8557-glitch <tigan8557@gmail.com>
2026-06-27 12:55:39 +0300

    Change Cloudinary credentials in api.js
    Updated Cloudinary configuration with new credentials.

--- routes/api.js
+++ routes/api.js
@@ -89,9 +89,9 @@ const sessionSchema = new mongoose.Schema({
 const Session = mongoose.models.Session || mongoose.model('Session', sessionSchema);
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dycytqdfj',
-    api_key: process.env.CLOUDINARY_API_KEY || '983286743251596',
-    api_secret: process.env.CLOUDINARY_API_SECRET || 'zeU4nedVzVzvqqndh2MF82AdRiI',
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dnugrwwlu',
+    api_key: process.env.CLOUDINARY_API_KEY || '871378976832311',
+    api_secret: process.env.CLOUDINARY_API_SECRET || '2aTYuPuNhkZD6Ytjg7YseGLVfb4',
     secure: true
 });
 
